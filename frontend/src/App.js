d936d054f107311d17fb86a0a4cdb25631e04fd5
rroyssse <rroyssse@gmail.com>
2023-06-08 16:25:41 +0300

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">UAFashion</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
