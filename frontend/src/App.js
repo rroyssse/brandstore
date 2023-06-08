da045fa8af3c977d470b8a438a119c70040be79a
rroyssse <rroyssse@gmail.com>
2023-06-08 17:15:18 +0300

    add product continue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,9 +13,18 @@ function App() {
         <div className="products">
           {data.products.map((product) => (
             <div className="product" key={product.slug}>
-              <img scr={product.image} alt={product.name} />
-              <p>{product.name}</p>
-              <p>{product.price}</p>
+              <a href={`/product/${product.slug}`}>
+                <img scr={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
           ))}
         </div>
