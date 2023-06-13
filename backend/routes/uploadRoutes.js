ef15e2d17c5840b688182faa391379e765ea651e
rroyssse <rroyssse@gmail.com>
2023-06-13 18:03:56 +0300

    add cloudinary

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
+import { isAdmin, isAuth } from '../utils.js';
+
+const upload = multer();
+
+const uploadRouter = express.Router();
+
+uploadRouter.post(
+  '/',
+  isAuth,
+  isAdmin,
+  upload.single('file'),
+  async (req, res) => {
+    cloudinary.config({
+      cloud_name: 'dezpuyp7p',
+      api_key: '552325339255376',
+      api_secret: 'u4TGkxSOHNbnL5ivgZ8VbZYTPg8',
+    });
+    const streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+    const result = await streamUpload(req);
+    res.send(result);
+  }
+);
+export default uploadRouter;
